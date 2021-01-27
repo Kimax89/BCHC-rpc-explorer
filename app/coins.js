c0a87c01a2423194742ed26886421462d0acc5b1
Klaus Kirkeby <klauskirkeby@hotmail.com>
2021-01-27 11:37:53 +0100

    Adding BCHC

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,11 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
+var bchc = require("./coins/bchc.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
+	"BCHC": bchc,
 
-	"coins":["BTC", "LTC"]
+	"coins":["BTC", "LTC", "BCHC"]
 };
